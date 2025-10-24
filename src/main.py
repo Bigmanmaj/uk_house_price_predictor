2d727d08801ab4fc1d5a63fd7e5192f7a7048f00
Maciej Roszkowski <ZMAC601@live.rhul.ac.uk>
2025-10-24 20:58:19 +0100

    Implemented initial polynomial model

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-'''From this module you can select different functionality of the app depending on input'''
+'''From this module you can select different
+functionality of the app depending on input'''
 
 
 if __name__ == '__main__':
-    pass
+    ''''''
--- src/visualiser.py
+++ src/visualiser.py
@@ -1,9 +1,5 @@
 '''Module focusing on visualising data'''
 
 
-import matplotlib
-
-
 def visualise_house_price():
     '''Visualise house price over time based on ONS dataset'''
-
